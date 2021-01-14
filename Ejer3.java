543a78a1cac34c9f6bbd93e6b4d8a3f51233deff
alexrp1900 <56952231+alexrp1900@users.noreply.github.com>
2021-01-14 13:23:59 +0100

    Add files via upload

---
+++ Ejer3.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Ejer3 {
+
+	public static void main(String[] args) {
+
+		int[] numero = new int[20];
+
+		int cont = 0;
+		int num = 0;
+		while (cont < 40) {
+			System.out.println(cont + num);
+
+			cont = cont + 2;
+
+		}
+
+	}
+}
